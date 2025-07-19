ce97659d23931d612c8093f00f4cbbd43cf3c543
srijana <b180008@nitsikkim.ac.in>
2025-07-19 17:00:23 +0530

    Fixed Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import TssApp from './pages/TSSApp';
-
-
-import Portfolio from './pages/Portfolio';
-import About from './pages/About';
-import Team from './pages/Team';
-import Testimonials from './pages/Testimonials' ;
-import Contact from './pages/Contact';
+import React from 'react';
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import TssApp from './pages/TSSApp'
+import Portfolio from './pages/Portfolio'
+import About from './pages/About'
+import Team from './pages/Team'
+import Testimonials from './pages/Testimonials' 
+import Contact from './pages/Contact'
 
 
 function App() {
